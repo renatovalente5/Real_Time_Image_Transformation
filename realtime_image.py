e16171d33834e4d2e4a8cfb151b7698649e9a33a
Renato Valente <renatovalente5@ua.pt>
2021-01-11 23:07:43 +0000

    Brightness added

--- realtime_image.py
+++ realtime_image.py
@@ -1,34 +1,50 @@
 import numpy as np
+import numpy as _is_numpy_image
 import cv2
 
-shot_idx = 0
+
 video = cv2.VideoCapture(0)
+
+shot_idx = 0
+brilho = 1
+
+def adjust_brightness(img, brightness_factor):
+    table = cv2.convertScaleAbs(img, alpha=brightness_factor, beta=0)
+    brightness = cv2.hconcat([frame, table])
+    cv2.imshow('brightness', brightness)
+
 
 while(True):
     # Capture frame-by-frame
     ret, frame = video.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    #cv2.imshow('frame', gray)
+    brightness = cv2.hconcat([frame, frame])
+    cv2.namedWindow('brightness')
+    adjust_brightness(frame, brilho)
 
     ch = 0xFF & cv2.waitKey(1)
     if ch == ord('q'):
         break
 
 
-## Key Press Actions
- 
+### Key Press Actions
+
+# Light
+    if ch == ord('+'):
+        brilho = brilho * 1.1
+    if ch == ord('-'):
+        brilho = brilho * 0.9
+
+        
+# Print and save image
     if ch == ord('p'):
-    
         fn = './prints/shot_%03d.bmp' % (shot_idx)
-        cv2.imwrite(fn, frame)
+        cv2.imwrite(fn, brightness)
         print(fn, 'saved')
         shot_idx += 1
- 
+
 
 # Definition of window size
     if ch == ord('2'):
