0b1250859d423ba3e6fa14fa21e8aff2acaa3262
Renato Valente <renatovalente5@ua.pt>
2021-01-12 02:58:26 +0000

    Letters Reader
    sudo pip3 install tesseract

--- realtime_image.py
+++ realtime_image.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+from PIL import Image
+import pytesseract
 
 
 video = cv2.VideoCapture(0)
@@ -8,23 +10,63 @@ shot_idx = 0
 brilho = 1
 gray_bool = False
 
-def adjust_brightness(img, brightness_factor):
-    table = cv2.convertScaleAbs(img, alpha=brightness_factor, beta=0)
+
+def adjust_brightness(frame2, brightness_factor):
+    table = cv2.convertScaleAbs(frame2, alpha=brightness_factor, beta=0)
     brightness = cv2.hconcat([frame, table])
     cv2.imshow('brightness', brightness)
 
+def salt_pepper(grayy, prob):
+      # Extract image dimensions
+      row, col, c = grayy.shape
+
+      # Declare salt & pepper noise ratio
+      s_vs_p = 0.5
+      output = np.copy(grayy)
+
+      # Apply salt noise on each pixel individually
+      num_salt = np.ceil(prob * grayy.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+            for i in grayy.shape]
+      output[coords] = 1
+
+      # Apply pepper noise on each pixel individually
+      num_pepper = np.ceil(prob * grayy.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+            for i in grayy.shape]
+      output[coords] = 0
+      cv2.imshow('output', output)
+
+      return output
+
+def limpaImg(img1, novaImg):
+    img = Image.open(img1)
+    img = img.point(lambda x: 0 if x<100 else 255)
+    img.save(novaImg)    
+    return img
+
+# def contraharmonic_mean(grayy, img, size, Q):
+#     num = np.power(grayy, Q + 1)
+#     denom = np.power(grayy, Q)
+#     kernel = np.full(size, 1.0)
+#     result = cv2.filter2D(num, -1, kernel) / cv2.filter2D(denom, -1, kernel)
+#     return result
 
 while(True):
     ret, frame = video.read() # Capture frame by frame of the WebCam
     brightness = cv2.hconcat([frame, frame])
+    frame2 = cv2.blur(frame, (2, 2))
+    
 
     if gray_bool == True:  #If TRUE show video in GRAY
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
         grayy = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+        # sp_05 = salt_pepper(grayy, 0.5)
+        # cv2.imshow('show', contraharmonic_mean(grayy, sp_05, (3,3), 0.5))
         adjust_brightness(grayy, brilho)
     else:
-        adjust_brightness(frame, brilho)
-
+        adjust_brightness(frame2, brilho)
+    
 
     ch = 0xFF & cv2.waitKey(1)  # Press 'q' to exit
     if ch == ord('q'):
@@ -51,9 +93,12 @@ while(True):
 # Print and save image
     if ch == ord('p'):
         fn = './prints/shot_%03d.bmp' % (shot_idx)
-        cv2.imwrite(fn, brightness)
+        cv2.imwrite(fn, frame)
         print(fn, 'saved')
         shot_idx += 1
+
+        image = limpaImg('./prints/shot_%03d.bmp' % (shot_idx-1), './prints/shot_%03dFilter.png' % (shot_idx-1))
+        print(pytesseract.image_to_string(image))
 
 
 # Definition of window size
