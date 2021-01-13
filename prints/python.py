8d50e5481bfa45d558e18688ba6514466be08827
Renato Valente <renatovalente5@ua.pt>
2021-01-13 04:41:53 +0000

    Mil e uma coisas (LINDO)
    Face Detection
    Auto Color
    Auto Img modifications with Histogram
    ...

--- prints/python.py
+++ prints/python.py
@@ -1,14 +1,29 @@
-
 import cv2
 import numpy as np
+from PIL import Image
+import pytesseract
 
-im = cv2.imread('shot_001.bmp')
-img = cv2.imread('shot_001.bmp',0)
+#img = cv2.imread("ImgLimpa.png")
 
-# Convert grayscale image to 3-channel image,so that they can be stacked together    
-imgc = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-both = np.hstack((im,imgc))
+def limpaImg(img1, novaImg):
+    img = Image.open(img1)    #Threshold para a imagem, e salva-la
+    img = img.point(lambda x: 0 if x<100 else 255)
+    img.save(novaImg)    
+    return img
 
-cv2.imshow('imgc',both)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+image = limpaImg('shot_000.bmp', 'ImgLimpa2.png')
+print(pytesseract.image_to_string(image))
+
+# def shadow_remove(img):
+#     rgb_planes = cv2.split(img)
+#     result_norm_planes = []
+#     for plane in rgb_planes:
+#         dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+#         bg_img = cv2.medianBlur(dilated_img, 21)
+#         diff_img = 255 - cv2.absdiff(plane, bg_img)
+#         norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+#         result_norm_planes.append(norm_img)
+#     shadowremov = cv2.merge(result_norm_planes)
+#     return shadowremov#Shadow removal
+# shad = shadow_remove(img)
+# cv2.imwrite('shot_0002.bmp', shad)
--- realtime_image.py
+++ realtime_image.py
@@ -2,19 +2,93 @@ import numpy as np
 import cv2
 from PIL import Image
 import pytesseract
+from matplotlib import pyplot as plt
 
-
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 video = cv2.VideoCapture(0)
 
 shot_idx = 0
 brilho = 1
+contraste = 0
 gray_bool = False
+detect_face_bool = False
+face_blur = 0
+face_blur = 0
+alpha_slider_max = 100
+old_face_blur = 0
+old_out_blur = 0
+auto_correction = 0
+cinzento = 0
 
+def adjust_brightness(frame2, brightness_factor=0, contrast_factor=0):
+    brightness_factor /= 100
+    contrast_factor -= 127
 
-def adjust_brightness(frame2, brightness_factor):
-    table = cv2.convertScaleAbs(frame2, alpha=brightness_factor, beta=0)
+    if brightness_factor != 0:
+        if brightness_factor > 0:
+            shadow = brightness_factor
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness_factor
+        alpha_b = (highlight - shadow)/255
+        gamma_b = shadow
+        buf = cv2.addWeighted(frame2, alpha_b, frame2, 0, gamma_b)
+    else:
+        buf = frame2.copy()
+
+    if contrast_factor != 0:
+        f = 131*(contrast_factor + 127)/(127*(131-contrast_factor))
+        alpha_c = f
+        gamma_c = 127*(1-f)
+
+        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+    
+    table = cv2.convertScaleAbs(buf, alpha=brightness_factor, beta=contrast_factor)
     brightness = cv2.hconcat([frame, table])
     cv2.imshow('brightness', brightness)
+
+    
+def blur_img(img, factor = 20):
+   kW = int(img.shape[1] / factor)
+   kH = int(img.shape[0] / factor)
+    
+   #ensure the shape of the kernel is odd
+   if kW % 2 == 0: kW = kW - 1
+   if kH % 2 == 0: kH = kH - 1
+    
+   blurred_img = cv2.GaussianBlur(img, (kW, kH), 0)
+   return blurred_img
+
+def extract_indexes(length, step_size):
+   begin=0
+   end=0
+   indexes = []
+    
+   cycles = int(length / step_size)
+    
+   for i in range(cycles):
+       begin = i * step_size
+       end = i * step_size+step_size
+       #print(i, ". [",begin,", ",end,")")
+    
+       index = []
+       index.append(begin)
+       index.append(end)
+       indexes.append(index)
+    
+       if begin >= length: break
+       if end > length: end = length
+    
+   if end < length:
+       #print(i+1,". [", end,", ",length,")")
+       index = []
+       index.append(end)
+       index.append(length)
+       indexes.append(index)
+    
+   return indexes
+
 
 def salt_pepper(grayy, prob):
       # Extract image dimensions
@@ -45,28 +119,132 @@ def limpaImg(img1, novaImg):
     img.save(novaImg)    
     return img
 
-# def contraharmonic_mean(grayy, img, size, Q):
-#     num = np.power(grayy, Q + 1)
-#     denom = np.power(grayy, Q)
-#     kernel = np.full(size, 1.0)
-#     result = cv2.filter2D(num, -1, kernel) / cv2.filter2D(denom, -1, kernel)
-#     return result
+
+def nothing(x):
+    pass
+
+cv2.namedWindow('brightness')
+cv2.createTrackbar('Luminosidade','brightness',100,200,nothing)  #o Brilho varia entre 0 e 1
+cv2.createTrackbar('Contraste','brightness',127,252,nothing)     #o contraste varia enter -127 e + 127
+cv2.createTrackbar('Detecting Face', 'brightness',0,1,nothing)
+cv2.createTrackbar('Background Blur', 'brightness',0,1,nothing)
+cv2.createTrackbar('Face Blur','brightness',0,40,nothing)
+cv2.createTrackbar('Global Blur','brightness',0,20,nothing)
+cv2.createTrackbar('Cores <-> Cinzento','brightness',0,1,nothing)
+cv2.createTrackbar('Auto-Correction','brightness',0,1,nothing)
+
 
 while(True):
     ret, frame = video.read() # Capture frame by frame of the WebCam
     brightness = cv2.hconcat([frame, frame])
-    frame2 = cv2.blur(frame, (2, 2))
+    global_blur = cv2.getTrackbarPos('Global Blur','brightness')
+    if global_blur == 0:
+        global_blur = 1
+    frame2 = cv2.blur(frame, (global_blur, global_blur))
+    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    brilho = cv2.getTrackbarPos('Luminosidade','brightness')
+    contraste = cv2.getTrackbarPos('Contraste','brightness')
+    out_blur = cv2.getTrackbarPos('Background Blur','brightness')
+    face_blur = cv2.getTrackbarPos('Face Blur','brightness')
+    auto_correction = cv2.getTrackbarPos('Auto-Correction','brightness')
+    cinzento = cv2.getTrackbarPos('Cores <-> Cinzento','brightness')
+    if old_face_blur != face_blur:                              #Altera entre o Blur do background e da cara
+        out_blur = 0
+        cv2.setTrackbarPos('Background Blur','brightness', 0)
+    if old_out_blur != out_blur:
+        face_blur = 0
+        cv2.setTrackbarPos('Face Blur','brightness', 0)
+
+    old_face_blur = face_blur
+    old_out_blur = out_blur
+
+    #Correção Automatica dos Histogramas com o equalizeHist
+    img_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
     
 
-    if gray_bool == True:  #If TRUE show video in GRAY
-        gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-        grayy = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+    faces = faceCascade.detectMultiScale(   #Deteção da Face
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+        )
+    
+    if detect_face_bool == True or cv2.getTrackbarPos('Detecting Face','brightness') == 1:
+        for (x, y, w, h) in faces:      # Draw a rectangle around the faces
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    
+    if gray_bool == True or cinzento == 1:  #If TRUE show video in Cinzento
+        if auto_correction == 1:
+            grayimg = cv2.cvtColor(img_output, cv2.COLOR_BGR2GRAY)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))  #Coreeção CLAHE (Contrast Limited Adaptive Histogram Equalization) 
+            cl1 = clahe.apply(grayimg)
+            grayy = cv2.cvtColor(cl1,cv2.COLOR_GRAY2RGB)
+        else:
+            grayy = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
         # sp_05 = salt_pepper(grayy, 0.5)
         # cv2.imshow('show', contraharmonic_mean(grayy, sp_05, (3,3), 0.5))
-        adjust_brightness(grayy, brilho)
+        if out_blur == 1:
+            blurred_img = blur_img(grayy, factor = 10)
+            for x, y, w, h in faces:
+                detected_face = grayy[int(y):int(y+h), int(x):int(x+w)]
+                blurred_img[y:y+h, x:x+w] = detected_face
+            adjust_brightness(blurred_img, brilho, contraste)
+        else:
+            if face_blur > 0:
+                for x, y, w, h in faces:
+                    detected_face = grayy[int(y):int(y+h), int(x):int(x+w)]
+                    pixelated_face = detected_face.copy()
+                    
+                    width = pixelated_face.shape[0]
+                    height = pixelated_face.shape[1]
+
+                    step_size = cv2.getTrackbarPos('Face Blur','brightness')
+
+                    for wi in extract_indexes(width, step_size):
+                        for hi in extract_indexes(height, step_size):
+                            detected_face_area = detected_face[wi[0]:wi[1], hi[0]:hi[1]]
+                            if detected_face_area.shape[0] > 0 and detected_face_area.shape[1] > 0:
+                                detected_face_area = blur_img(detected_face_area, factor = 0.5)
+                                pixelated_face[wi[0]:wi[1], hi[0]:hi[1]] = detected_face_area
+                    grayy[y:y+h, x:x+w] = pixelated_face
+                adjust_brightness(grayy, brilho, contraste)
+            else:
+                adjust_brightness(grayy, brilho, contraste)
     else:
-        adjust_brightness(frame2, brilho)
-    
+        if auto_correction == 1:
+            frame2 = img_output
+        if out_blur == 1:
+            blurred_img = blur_img(frame2, factor = 10)
+            for x, y, w, h in faces:
+                detected_face = frame2[int(y):int(y+h), int(x):int(x+w)]
+                blurred_img[y:y+h, x:x+w] = detected_face
+            adjust_brightness(blurred_img, brilho, contraste)
+        else:
+            if face_blur > 0:
+
+                for x, y, w, h in faces:
+                    detected_face = frame2[int(y):int(y+h), int(x):int(x+w)]
+                    pixelated_face = detected_face.copy()
+                    
+                    width = pixelated_face.shape[0]
+                    height = pixelated_face.shape[1]
+
+                    step_size = cv2.getTrackbarPos('Face Blur','brightness')
+
+                    for wi in extract_indexes(width, step_size):
+                        for hi in extract_indexes(height, step_size):
+                            detected_face_area = detected_face[wi[0]:wi[1], hi[0]:hi[1]]
+                            if detected_face_area.shape[0] > 0 and detected_face_area.shape[1] > 0:
+                                detected_face_area = blur_img(detected_face_area, factor = 0.5)
+                                pixelated_face[wi[0]:wi[1], hi[0]:hi[1]] = detected_face_area
+                    frame2[y:y+h, x:x+w] = pixelated_face
+                adjust_brightness(frame2, brilho, contraste)
+            else:
+                adjust_brightness(frame2, brilho, contraste)
+
 
     ch = 0xFF & cv2.waitKey(1)  # Press 'q' to exit
     if ch == ord('q'):
@@ -74,20 +252,20 @@ while(True):
 
 
 ### Key Press Actions
-
-# Light
-    if ch == ord('+'):
-        brilho = brilho * 1.1
-    if ch == ord('-'):
-        brilho = brilho * 0.9
-
-
 # Colors
     if ch == ord('g'):  #Image to GRAY
         gray_bool = True
 
     if ch == ord('c'):  ##Image with COLORS
         gray_bool = False
+
+# Detect
+    if ch == ord('d'):  ##Image with square face
+        detect_face_bool = True
+
+    if ch == ord('f'):  ##Image without square face
+        detect_face_bool = False
+
 
 
 # Print and save image
@@ -113,6 +291,7 @@ while(True):
     if ch == ord('4'):
         video.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
         video.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+
 
 # When everything done, release the capture
 video.release()
